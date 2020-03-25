d677f04478f78b57bd900b063ac862e3f75ab4b8
alekhyajaddu <alekhyajaddu@gmail.com>
2020-03-24 20:54:12 -0500

    modified

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const hostname = process.env.hostname || '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+  res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain');
   res.end('node server');
 });
